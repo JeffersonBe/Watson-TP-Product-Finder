173b6bb9093c3b94e062d24efb95d65516dbe7b7
Jefferson Bonnaire <jefferson.bonnaire@gmail.com>
2016-12-20 17:40:27 +0000

    Initial Project 🚀

--- tp-product-finder/ViewController.swift
+++ tp-product-finder/ViewController.swift
@@ -7,19 +7,109 @@
 //
 
 import UIKit
+import AVFoundation
+import SafariServices
+import Alamofire
+import SwiftyJSON
 
-class ViewController: UIViewController {
+let WatsonAPIKey = ""
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, SFSafariViewControllerDelegate {
+    
+    @IBOutlet weak var cameraView: UIView!
+    var safariVC: SFSafariViewController?
+    var media: UIImage!
+    var newMedia: Bool?
+    
+    let captureSession = AVCaptureSession()
+    let stillImageOutput = AVCapturePhotoOutput()
+    var error: NSError?
+    
+    @IBAction func useCamera(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(
+            UIImagePickerControllerSourceType.camera) {
+            
+            let imagePicker = UIImagePickerController()
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType =
+                UIImagePickerControllerSourceType.camera
+            
+            imagePicker.allowsEditing = false
+            
+            self.present(imagePicker, animated: true,
+                         completion: nil)
+            newMedia = true
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func showPageResultWith(keyword: String) {
+        let url = NSURL(string: "https://www.travisperkins.co.uk/search?text=\(keyword)")
+        safariVC = SFSafariViewController(url: url as! URL)
+        safariVC!.delegate = self
+        self.present(safariVC!, animated: true, completion: nil)
     }
+    
+    func calltoIBM() {
+        let URL = try! URLRequest(
+            url: "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify?api_key=\(WatsonAPIKey)&version=2016-05-20",
+            method: .post,
+            headers: ["Accept-Language":"en"]
+        )
 
+        Alamofire.upload(multipartFormData: { (multipartFormData) in
+            if let imageData = UIImageJPEGRepresentation(self.media, 0.5) {
+                multipartFormData.append(imageData,
+                                         withName: "images_file",
+                                         fileName: "photo.jpeg",
+                                         mimeType: "image/jpeg")
+            }
+        }, with: URL, encodingCompletion: { (result) in
+            switch result {
+            case .success(let upload, _, _):
+                upload.responseJSON { response in
+                    let json = JSON(data: response.data!)
+                    if let keyword = json["images"][0]["classifiers"][0]["classes"][0]["class"].string {
+                        self.showPageResultWith(keyword: keyword)
+                    }
+                }
 
+            case .failure(let encodingError):
+                print(encodingError)
+            }
+        })
+    }
+    
+    // Delegate
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        print("imagePickerController")
+        media = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        self.dismiss(animated: true, completion: {
+            self.calltoIBM()
+        })
+    }
+    
+    func image(image: UIImage, didFinishSavingWithError error: NSErrorPointer, contextInfo:UnsafeRawPointer) {
+        guard error == nil else {
+            let alert = UIAlertController(title: "Save Failed",
+                                          message: "Failed to save image",
+                                          preferredStyle: UIAlertControllerStyle.alert)
+            
+            let cancelAction = UIAlertAction(title: "OK",
+                                             style: .cancel, handler: nil)
+            
+            alert.addAction(cancelAction)
+            self.present(alert, animated: true,
+                         completion: nil)
+            return
+        }
+    }
+    
+    // Safari Delegate
+    func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
+        controller.dismiss(animated: true, completion: nil)
+    }
 }
 
